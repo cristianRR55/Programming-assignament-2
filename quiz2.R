963893e87f01468f4f5ca308b8cf95fac6a50229
cristianRR55 <1803255@unimilitar.edu.co>
2020-09-27 22:01:40 -0500

    codigo1

--- quiz2.R
+++ quiz2.R
@@ -1,19 +1,19 @@
-makeCacheMatrix <- function( m = matrix() ) {
-  i <- NULL
-  set <- function( matrix ) {
+makeCacheMatrix <- function( m = matrix() ) { ##Crear un objeto de matriz especial que puede almacenar en caché su inverso
+  i <- NULL #inciar inversa
+  set <- function( matrix ) { #setear la matrix
     m <<- matrix
     i <<- NULL
   }
-  get <- function() {
-    m
+  get <- function() { #traer la matrix
+    m #retornar matrix
   }
-  setInverse <- function(inverse) {
+  setInverse <- function(inverse) {#setear inversa de la matrix
     i <<- inverse
   }
-  getInverse <- function() {
+  getInverse <- function() {#traer matrix inversa
     i
   }
-  list(set = set, get = get,
+  list(set = set, get = get, #retornar lista de metodos 
        setInverse = setInverse,
        getInverse = getInverse)
 }
@@ -26,8 +26,8 @@ cacheSolve <- function(x, ...) {
     message("Obteniendo datos")
     return(m)
   }
-  data <- x$get()
-  m <- solve(data) %*% data
-  x$setInverse(m)
-  m
+  data <- x$get() #traer la matrix de nuestros objetos 
+  m <- solve(data) %*% data #calcular inversa utilizando multiplicaciones de matrices
+  x$setInverse(m)# establecer la inversa del objeto
+  m #retirnar matrix
 }
